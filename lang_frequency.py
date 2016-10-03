86bcb8bb7dd2ee88c16da4853a0aea6cb66be3cb
SBKubric <sbkubric@gmail.com>
2016-10-03 20:03:16 +0300

    Solved the issue

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,29 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if filepath != '':
+        with open(filepath) as data_file:
+            data = data_file.read()
+    else:
+        data = -1
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'\w+', text)
+    cap_words = [word.upper() for word in words]
+    word_counts = Counter(cap_words)
 
+    s = 'Most common: '
+    for letter, count in word_counts.most_common(10):
+        s = format('%s%s ' % (s, letter.lower()))
+    print(s)
 
 if __name__ == '__main__':
-    pass
+    print('Enter the filepath:')
+    text = -1
+    while text == -1:
+        text = load_data(input())
+    get_most_frequent_words(text)
